37d3c174324835b08bf81204ce67f3541bd3a36e
leabl393 <leabl393@sudent.otago.ac.nz>
2023-10-05 03:27:59 +0000

    first comit

---
+++ retrieve.c
@@ -0,0 +1,17 @@
+
+#include <libmemdrv.h>
+
+// open 
+open_device();
+
+// loop over values in iNode and read them each 
+for( int i = 1 ; i < MAX_BID ; i++){
+    
+    read_block(i, )
+
+}
+
+// close 
+close_device();
+
+
